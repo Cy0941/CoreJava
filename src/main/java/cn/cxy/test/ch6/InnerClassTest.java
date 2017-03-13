f583fb0201f01d7c639d6d87b38a6f28bf7ddd49
cx_yang <74914952>
2017-03-13 22:29:39 +0800

    内部类（TODO）

--- src/main/java/cn/cxy/test/ch6/InnerClassTest.java
+++ src/main/java/cn/cxy/test/ch6/InnerClassTest.java
@@ -9,20 +9,22 @@ import java.util.Date;
 /**
  * Function: TODO
  * Reason: TODO ADD REASON(可选).</br>
- * Date: 2017/2/27 23:26 </br>
+ * Date: 2017/3/4 12:19 </br>
  *
  * @author: cx.yang
  * @since: Thinkingbar Web Project 1.0
  */
 public class InnerClassTest {
+
     public static void main(String[] args){
-        TalkingClock clock = new TalkingClock(3000,true);
-        //TODO 创建内部类的方式
-        TalkingClock.TimePrinter printer = clock.new TimePrinter();
-        clock.start();
-        JOptionPane.showMessageDialog(null,"Quit Program?");
+        TalkingClock clock = new TalkingClock(1000,true);
+        //TalkingClock.TimePrinter printer = clock.new TimePrinter();
+        //clock.start();
+        clock.start(1000,true);
+        JOptionPane.showMessageDialog(null,"Quit program?");
         System.exit(0);
     }
+
 }
 
 class TalkingClock{
@@ -34,26 +36,44 @@ class TalkingClock{
         this.beep = beep;
     }
 
-    public void start(){
-        //TODO
-        this.new TimePrinter();
-
-        ActionListener listener = new TimePrinter();
+    public void start(int interval,final boolean beep){
+        //TODO 内部类的创建必须依赖外部类（显示或隐式的标志）
+        //ActionListener listener1 = this.new TimePrinter();
+        //ActionListener listener0 = TalkingClock.this.new TimePrinter();
+        TalkingClock.TimePrinter listener = new TimePrinter();
         Timer t = new Timer(interval,listener);
         t.start();
+
+        //TODO 局部内部类 局部类不能使用 public 或 private 访问说明符进行说明
+        class TimePrinter2 implements ActionListener{
+            public void actionPerformed(ActionEvent e) {
+                Date now = new Date();
+                System.out.println("At the tone,the time is "+now);
+                //TODO 引用外部类的参数
+                //if (beep){
+                //if (TalkingClock.this.beep){
+                //TODO final
+                if (beep){
+                    Toolkit.getDefaultToolkit().beep();
+                }
+            }
+        }
+
+        ActionListener listener2 = new TimePrinter2();
+        Timer t2 = new Timer(1000,listener2);
+        t2.start();
     }
 
     /**
-     * 此类随所在类方法创建（不是所有的所在类都含有此内部类对象）
-     * TODO 只有内部类可以是 私有private 的
+     * 内部类
      */
     public class TimePrinter implements ActionListener{
 
         public void actionPerformed(ActionEvent e) {
             Date now = new Date();
             System.out.println("At the tone,the time is "+now);
-            if (TalkingClock.this.beep){
             //if (beep){
+            if (TalkingClock.this.beep){
                 Toolkit.getDefaultToolkit().beep();
             }
         }
--- src/main/java/cn/cxy/test/ch6/MainTest.java
+++ src/main/java/cn/cxy/test/ch6/MainTest.java
@@ -1,6 +1,8 @@
 package cn.cxy.test.ch6;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,5 +24,21 @@ public class MainTest {
         for (String s : list){
             System.out.println(s);
         }
+
+        //TODO 看不懂啊
+        //int count = 0;
+        final int[] counter = new int[1];
+        Date[] dates = new Date[100];
+        for (int i = 0; i < dates.length; i++)
+            dates[i] = new Date(){
+                public int compareTo(Date other){
+                    //count ++;
+                    counter[0]++;
+                    return super.compareTo(other);
+                }
+            };
+        Arrays.sort(dates);
+        //System.out.println(count + " comparisons.");
+        System.out.println(counter[0]+" comparisons.");
     }
 }
