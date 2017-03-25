3206ee985d0f7b8bd532a310f98dfe8902e11307
cx_yang <84593cxy>
2017-03-25 10:28:41 +0800

    bug fixed

--- src/main/java/cn/cxy/test/ch14_thread/Bank.java
+++ src/main/java/cn/cxy/test/ch14_thread/Bank.java
@@ -1,5 +1,8 @@
 package cn.cxy.test.ch14_thread;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Function: TODO
  * Reason: TODO ADD REASON(可选).</br>
@@ -11,31 +14,48 @@ package cn.cxy.test.ch14_thread;
 public class Bank {
 
     private final double[] accounts;
+    /**
+     * 锁 - 同步访问机制实现
+     */
+    private Lock bankLock;
 
     public Bank(int n, double initialBalance) {
         accounts = new double[n];
         for (int i = 0; i < n; i++) {
             accounts[i] = initialBalance;
         }
+        //TODO 锁
+        bankLock = new ReentrantLock();
     }
 
     public void transfer(int from, int to, double amount) {
-        if (accounts[from] < amount) {
-            return;
+        bankLock.lock();
+        try {
+            if (accounts[from] < amount) {
+                return;
+            }
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf("%10.2f from %d to %d ", amount, from, to);
+            System.out.println();
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+        }finally {
+            bankLock.unlock();
         }
-        System.out.println(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf("%10.2f from %d to %d , and totalBalance is : %s", amount, from, to,getTotalBalance());
-        System.out.println();
-        accounts[to] += amount;
     }
 
     public double getTotalBalance() {
-        double sum = 0;
-        for (double account : accounts) {
-            sum += account;
+        bankLock.lock();
+        try {
+            double sum = 0;
+            for (double account : accounts) {
+                sum += account;
+            }
+            return sum;
+        }finally {
+            bankLock.unlock();
         }
-        return sum;
     }
 
     public int size() {
